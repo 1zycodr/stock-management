1745100bc6b4fc03f5655d2884b341bdcad96b30
1zycodr <5zamiggos@gmail.com>
2022-05-19 12:39:04 +0600

    Initial commit

---
+++ src/test/java/kz/iitu/itse1909r/mukhitdinov/StockManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.itse1909r.mukhitdinov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
